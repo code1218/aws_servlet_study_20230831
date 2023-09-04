6349fcbf8efc693906cfce5e057f143f80470c16
code1218 <skjil1218@gmail.com>
2023-09-04 16:56:42 +0900

    signin

--- src/main/java/sevlet/SigninServlet.java
+++ src/main/java/sevlet/SigninServlet.java
@@ -1,6 +1,7 @@
 package sevlet;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Objects;
 
 import javax.servlet.ServletException;
@@ -11,19 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import data.UserData;
 import entity.User;
+import utils.JsonParseUtil;
 import utils.ResponseUtil;
 
-@WebServlet("/auth/signup/duplicate/username")
-public class DuplicateUsername extends HttpServlet {
+@WebServlet("/auth/signin")
+public class SigninServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String username = request.getParameter("username");
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Map<String, Object> signinUser = JsonParseUtil.toMap(request.getInputStream());
 		Boolean responseData = false;
 		
+		System.out.println(signinUser);
+		
 		for(User user : UserData.userList) {
-			if(Objects.equals(user.getUsername(), username)) {
+			if(Objects.equals(user.getUsername(), signinUser.get("username"))
+					&& Objects.equals(user.getPassword(), signinUser.get("password"))) {
 				responseData = true;
 				break;
 			}
@@ -33,3 +37,11 @@ public class DuplicateUsername extends HttpServlet {
 	}
 
 }
+
+
+
+
+
+
+
+
